9ae41ed6224797974bbf344bfb16b0ce26c49910
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-09 14:14:21 -0300

    Commit inicial

---
+++ src/main/java/com/bluewolf/ponto/api/PontointeligenteApiApplication.java
@@ -0,0 +1,13 @@
+package com.bluewolf.ponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluewolf/ponto/api/PontointeligenteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluewolf.ponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
