e503516b19f1e1b7257f92f274908a3df3fe9af9
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-10 16:49:25 -0300

    Criação teste

---
+++ src/test/java/com/bluewolf/ponto/Repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.bluewolf.ponto.Repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import com.bluewolf.ponto.api.PontointeligenteApiApplicationTests;
+import com.bluewolf.ponto.models.Empresa;
+import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = {EmpresaRepository.class})
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public class EmpresaRepositoryTest {
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    private static final String CNPJ = "51463645000100";
+
+    @Before
+    public void setUp() throws Exception {
+        Empresa empresa = new Empresa();
+        empresa.setRazaoSocial("Empresa de exemplo");
+        empresa.setCnpj(CNPJ);
+        this.empresaRepository.save(empresa);
+    }
+
+    @After
+    public final void tearDown() {
+        this.empresaRepository.deleteAll();
+    }
+
+    @Test
+    public void testBuscarPorCnpj() {
+        Empresa empresa = this.empresaRepository.findByCnpj(CNPJ);
+
+        assertEquals(CNPJ, empresa.getCnpj());
+    }
+
+}
