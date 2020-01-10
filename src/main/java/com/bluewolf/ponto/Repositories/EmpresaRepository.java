257ad816752f72925d5418ba5d5e55c7cb226710
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-10 14:36:26 -0300

    Criação dos repositórios

---
+++ src/main/java/com/bluewolf/ponto/Repositories/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.bluewolf.ponto.Repositories;
+
+import com.bluewolf.ponto.models.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    @Transactional(readOnly = true)
+    Empresa findByCnpj(String cnpj);
+
+}
---
+++ src/main/java/com/bluewolf/ponto/Repositories/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package com.bluewolf.ponto.Repositories;
+
+import com.bluewolf.ponto.models.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    Funcionario findByCpf(String cpf);
+
+    Funcionario findByEmail(String email);
+
+    Funcionario findByCpfOrEmail(String cpf, String email);
+}
